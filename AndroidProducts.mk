0ce12ae409269509028fa6dbedb8ced71aabcefb
Deivid21 <david.parra.ignacio@gmail.com>
2021-02-21 16:03:39 -0300

    channel: AOSiP-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_channel.mk
+    $(LOCAL_DIR)/aosip_channel.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_channel-eng \
-    lineage_channel-user \
-    lineage_channel-userdebug
+    aosip_channel-eng \
+    aosip_channel-user \
+    aosip_channel-userdebug
